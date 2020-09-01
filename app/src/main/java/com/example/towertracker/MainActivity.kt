0fd91f656b97c3bc17a54afbe4bf8995656a8cdf
Himanshu Sharma <samitihs4@gmail.com>
2020-09-02 00:09:57 +0530

    - removed firebase RTD + switched to own mysql server

--- app/src/main/java/com/example/towertracker/MainActivity.kt
+++ app/src/main/java/com/example/towertracker/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.towertracker
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.textfield.TextInputLayout
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+
 
 private lateinit var database: DatabaseReference
 lateinit var passwordTextField: TextInputLayout
@@ -39,7 +38,35 @@ class MainActivity : AppCompatActivity() {
             val user = usernameTextField.editText?.text.toString()
             val pass = passwordTextField.editText?.text.toString()
 
-            val listener = object : ValueEventListener {
+            val queue = Volley.newRequestQueue(applicationContext)
+
+            val jsonObjectRequest = object :
+                StringRequest(Method.POST, LOGIN,
+                    Response.Listener {
+                        try {
+                            Toast.makeText(this@MainActivity, "data: $it", Toast.LENGTH_SHORT)
+                                .show()
+                        } catch (e: Exception) {
+                            Toast.makeText(this@MainActivity, "exception: $e", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+
+                    }, Response.ErrorListener {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Volley error occurred: $it",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }) {
+                override fun getParams(): MutableMap<String, String> {
+                    val params = HashMap<String, String>()
+                    params["username"] = user
+                    return params
+                }
+            }
+            queue.add(jsonObjectRequest)
+
+            /*val listener = object : ValueEventListener {
                 override fun onDataChange(dataSnapshot: DataSnapshot) {
 
                     if (dataSnapshot.exists()) {
@@ -70,8 +97,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             database.child("auth").orderByChild("username").equalTo(user)
-                .addListenerForSingleValueEvent(listener)
-
+                .addListenerForSingleValueEvent(listener)*/
 
         }
     }
